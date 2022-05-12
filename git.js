59e5ed8468ac04d9ab25f35bfc7c423bcfdc16df
Makhabat Berdibekova <makhabat.berdibekova.kk@gmail.com>
2022-05-12 12:20:16 +0600

    finish

--- git.js
+++ git.js
@@ -1,18 +1,18 @@
 class Github {
     constructor() {
-        this.client_id = "4673cde040b6a9c1267c";
-        this.client_secret = "b9bc7e1bbcd5d1c5f27c489ec3cd273c4b90da21";
+      this.client_id = "f09a6225a36f5f2a9bd2";
+      this.client_secret = "cf2d6368a6d22051a8de61ed325c924588ff03aa";
     }
-
+  
     async getUser(user) {
-        const profileResponse = await fetch (
-            `https://api.github.com/users/$(user)?client_id=${this.client_id}&client_secret=${this.client_secret}`
-        );
-
-        const profile = await profileResponse.json();
-
-        return {
-            profile: profile,
-        };
+      const profileResponse = await fetch(
+        `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
+      );
+  
+      const profile = await profileResponse.json();
+  
+      return {
+        profile: profile,
+      };
     }
-}
+  }
